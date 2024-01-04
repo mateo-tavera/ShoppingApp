55ff2412b4289088465de8cb223bfc76550eaa6f
Mateo Tavera <mateo.tavera8@gmail.com>
2024-01-04 11:10:22 +0000

    fix package

---
+++ service/article.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	dbc "github.com/mateo-tavera/shoppingCart/dbConnection"
+)
+
+type ArticleList struct {
+	Id    string `json:"id"`
+	Title string `json:"title"`
+	Price string `json:"price"`
+	Qty   int    `json:"qty"`
+}
+
+func GetArticles() func(qty, item int) []ArticleList {
+
+	var listOfArticles []ArticleList
+
+	//Using the GET method, we obtain the article list form API
+	url := "http://challenge.getsandbox.com/articles"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Cannot Get the url", err)
+	}
+	defer response.Body.Close()
+
+	//Store the data in a byte slice
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Cannot read the body", err)
+	}
+
+	if err := json.Unmarshal(content, &listOfArticles); err != nil {
+		panic(err)
+	}
+
+	//Convert into type ArticleList
+	err = json.Unmarshal(content, &listOfArticles)
+	if err != nil {
+		log.Fatal("Cannot parse the JSON", err)
+	}
+
+	SentToDatabase(listOfArticles)
+	var items4Cart []ArticleList
+
+	//Now that we have the list, we select which and how many articles we want
+	return func(qty, item int) []ArticleList {
+		listOfArticles[item].Qty = qty
+		items4Cart = append(items4Cart, listOfArticles[item])
+		return items4Cart
+	}
+
+}
+
+func SentToDatabase(listOfArticles []ArticleList) {
+	Db, err := dbc.GetDBConnection()
+	if err != nil {
+		log.Fatal("Cannot get DB connection", err)
+	}
+
+	for _, item := range listOfArticles {
+		_, err = Db.Exec("INSERT INTO articles (id_article, title, price) VALUES (?, ?, ?)",
+			item.Id, item.Title, item.Price)
+		if err != nil {
+			log.Fatal("Cannot execute query:", err)
+		}
+	}
+	log.Println("Data successfully sent to database")
+
+}
